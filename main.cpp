602c5a3206f47533b6174ecd6168b1ec14ca5856
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2023-12-07 22:35:33 +0200

    handled missing last token, handled = splitting in parsing, added test.txt file

--- main.cpp
+++ main.cpp
@@ -6,80 +6,49 @@
 
 using namespace std;
 int main(){
-
-    // string file = "D:\\Engineer\\Level 4\\semester 9\\compilers\\project\\Lexical-Analyzer-Generator\\rules.txt";
     string file = "rules.txt";
     string content = Utils::readFile(file);
-    cout<<content<<endl;
     InputParser ip;
     char delimiter = '\n';
 
     std::vector<std::string> lines = Utils::splitString(content, delimiter);
 
     for (int i=0;i<lines.size();i++){
-        cout<<"\nLINE " <<lines[i]<<endl;
         ip.tokenize(lines[i],i);
         cout<<endl;
     }
     int i=0;
     std::vector<NFA> allNFAs = ip.allNFAs;
-    //     for ( auto& nfa : allNFAs) {
-    //         std::cout << "NFA index " <<i << std::endl;
-    //         std::cout << "NFA start state : " << nfa.getStartState().getId() << ", " << nfa.getStartState().getIsAccepting()<< std::endl;
-    //         std::cout << "NFA final state: " << nfa.getFinalState().getId() << ", " << nfa.getFinalState().getIsAccepting()<< std::endl;
-    //         std::map<int, State> statesMap = nfa.getStatesMap();
-    //         std::cout << "statesMap size: " << statesMap.size() << std::endl;
-    //         for ( auto& state : statesMap) {
-    //             std::cout << "      state: " << state.first << ", " << state.second.getIsAccepting() << std::endl;
-    //         }
-    //         std::cout << "------------------------------------------------------------------" << std::endl;
-    //         i++;
-    //     }
     NFA n = NFAGenerator::getFullNFA(ip.allNFAs) ;
-       std::cout << "start state: " << n.getStartState().getId() << ", " << n.getStartState().getIsAccepting()<< std::endl;
-       std::cout << "final state: " << n.getFinalState().getId() << ", " << n.getFinalState().getIsAccepting()<< std::endl;
-       std::map<int, State> statesMap = n.getStatesMap();
-       std::cout << "statesMap size: " << statesMap.size() << std::endl;
-       for ( auto& state : statesMap) {
-           std::cout << "state: " << state.first << ", " << state.second.getIsAccepting() << std::endl;
-           state.second.printTransitions();
-       }
-    
-       std::cout <<"Varvoura Hya alsbbb " << std::endl;
-    std::cout << "NFA states : " << n.getStatesMap().size() << std::endl;
-
-
     SubsetConstruction s(n);
-    // s.setAlphabet({
-    //     'a', 'b', 'c', 'd', 'e', 'f',
-    //     'g', 'h', 'i', 'j', 'k', 'l',
-    //     'm', 'n', 'o', 'p', 'q', 'r',
-    //     's', 't', 'u', 'v', 'w', 'x',
-    //     'y', 'z',
-    //     'A', 'B', 'C', 'D', 'E', 'F',
-    //     'G', 'H', 'I', 'J', 'K', 'L',
-    //     'M', 'N', 'O', 'P', 'Q', 'R',
-    //     'S', 'T', 'U', 'V', 'W', 'X',
-    //     'Y', 'Z',
-    //     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    //     '>','=','<','!',';', ',', '(', ')', '{', '}',
-    //     '+', '-', '*', '/', EPSILLON});
-    // std::map< int , State> DFA = s.getDFA();
-    // std::cout << "DFA states size = " << DFA.size() << std::endl;
-
-
-    //   std::cout << "Dtrans new states :\n";
-    //   for ( const auto& transition : DFA) {
-    //       int id = transition.first;
-    //       State U = transition.second;
-    
-    //       std::cout << "state id " << id << " : " << std::endl;
-    //       U.printTransitions(1) ;
-    //       std::cout << "-------------------------"<< std::endl;
-    //   }
-
+    s.setAlphabet({
+        'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '>','=','<','!',';', ',', '(', ')', '{', '}',
+        '+', '-', '*', '/', EPSILLON});
+    std::map< int , State> DFA = s.getDFA();
+    std::cout << "DFA states size = " << DFA.size() << std::endl;
     std::map< int , State> minimizedDFA = s.minimizeDFA(DFA);
-   std::cout << "MinimizedDFA states size = " << minimizedDFA.size() << std::endl;
+    std::cout << "MinimizedDFA states size = " << minimizedDFA.size() << std::endl;
+
+    int startStateID = s.getStartStateID();
+    int deadStateID = s.getDeadStateID();
+    // string input = Utils::readFile("input.txt");
+    string input = Utils::readFile("test.txt");
+
+    vector<Token> tokens = LexicalAnalyzer::getTokens(input,startStateID,deadStateID,DFA);
+    for ( auto& token : tokens) {
+        std::cout << "token: " << token.type << " ( " << token.lexeme<< " ) " << std::endl;
+    }
    return 0;
 
 }
--- src/input_parser.h
+++ src/input_parser.h
@@ -1,36 +1,33 @@
 #ifndef INPUT_PARSER_H
 #define INPUT_PARSER_H
 
-
 #include <bits/stdc++.h>
 #include <algorithm>
 #include "token.h"
 #include "utils.h"
 #include "nfa.h"
 #include "infix_to_postfix.h"
-#include "regular_expression.h"
 #include "nfa_generator.h"
-class InputParser {
-    private:
-        static std::vector<Token*> tokens;
-        static const std::regex regDefRegex;
-        static const std::regex regExpRegex;
-        static const std::regex keywordRegex;
-        static const std::regex punctuationRegex;
-        static std::map<std::string, RegularExpression> regularDefinitions;
-        static std::vector<std::string> keywords;
-        static std::vector<std::string> punctuationSymbols;
+class InputParser
+{
+private:
+    static std::vector<Token *> tokens;
+    static const std::regex regDefRegex;
+    static const std::regex regExpRegex;
+    static const std::regex keywordRegex;
+    static const std::regex punctuationRegex;
+    static std::vector<std::string> keywords;
+    static std::vector<std::string> punctuationSymbols;
 
-        void parseRegDef(std::string);
-        void parseRegExp(std::string, int);
-        void parseKeyword(std::string);
-        void parsePunctuation(std::string);
-    public:
-        // InputParser();
-        std::vector<NFA> allNFAs;
-        void tokenize(std::string, int);
-        std::vector<std::string> generateInfix(std::string s);
-        // std::vector<Token> parseLexicalRulesFile(std::string filePath, std::vector<Token> *tokens , std::vector<std::string> * priorites, std::set<char> *alphabet);
+    void parseRegDef(std::string);
+    void parseRegExp(std::string, int);
+    void parseKeyword(std::string);
+    void parsePunctuation(std::string);
+
+public:
+    std::vector<NFA> allNFAs;
+    void tokenize(std::string, int);
+    std::vector<std::string> generateInfix(std::string s);
 };
 
-#endif  // INPUT_PARSER_H
+#endif // INPUT_PARSER_H
