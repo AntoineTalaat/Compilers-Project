0cbf35db5dcae9a19d02df8f46746c8f11dc3150
yarahossamboo <yarahossam222@gmail.com>
2023-12-26 04:43:04 +0200

    add project syntax rules

--- src/first_follow_builder.cpp
+++ src/first_follow_builder.cpp
@@ -10,6 +10,7 @@ FirstFollowBuilder::FirstFollowBuilder(std::map<std::string, std::vector<std::ve
     for(const auto nonTerminal : nonTerminals) {
         this->firstMap[nonTerminal] = {};
         this->followMap[nonTerminal] = {};
+        this->isFollowSet[nonTerminal] = false;
     }
 };
 
@@ -25,11 +26,6 @@ std::map<std::string, std::set<std::pair<std::string, std::vector<std::string>>>
 void FirstFollowBuilder::computeFirst(const std::string nonTerminal) {
     // iterate through each production of the correspoding current non-terminal productions
     for(const auto production : productions[nonTerminal]) {
-        std::cout << "Vector of strings:" << "  ";
-        for (const auto str : production) {
-            std::cout << str << " ";
-        }
-        std::cout << std::endl;
         // iterate through each symbol in the production
         for (const auto symbol : production) {
             // epsillon or terminal
@@ -37,6 +33,7 @@ void FirstFollowBuilder::computeFirst(const std::string nonTerminal) {
                 this->firstMap[nonTerminal].insert({symbol, std::vector<std::string>()});
                 break;
             }
+    
             else if (isTerminalString(symbol)) {
                 this->firstMap[nonTerminal].insert({symbol, production});
                 break;
@@ -64,63 +61,70 @@ void FirstFollowBuilder::computeFirst(const std::string nonTerminal) {
 std::map<std::string, std::set<std::string>> FirstFollowBuilder::getFollow() {
     this->followMap[Globals::START_SYMBOL].insert("$");
     
-    for(auto nonTerminal : this->nonTerminals)
-        computeFollow(nonTerminal);
+    for(auto production : this->productions)
+        if(!this->isFollowSet[production.first]) computeFollow(production.first);
 
     return this->followMap;
 }
 
 void FirstFollowBuilder::computeFollow(std::string nonTerminal) {
     // iterate through each production
-    for (auto rule : productions) {
+    for(auto rule : productions) {
         for(std::vector<std::string> production : rule.second) {
             for(int i=0; i<production.size(); i++) {
                 if(production[i] == nonTerminal) {
                    // law ana fe a5er el production
                     if(i == production.size() - 1) {
-                        auto lhsOfRule = rule.first;
-                        if(lhsOfRule == nonTerminal)    continue;
-                        computeFollow(lhsOfRule);
-                        for(auto follow : this->followMap[lhsOfRule]) {
-                            this->followMap[nonTerminal].insert(follow);
-                        }
+                        handleFollowLast(rule.first, nonTerminal);
                     }
                     else if(isTerminalString(production[i+1])) {
                         this->followMap[nonTerminal].insert(production[i+1]);
                     }
                     else {
-                        auto lhsOfRule = rule.first;
-                        for(int j = i + 1; j<production.size(); j++) {
-                            std::string nextSymbol = production[j];
-                            if(isTerminalString(nextSymbol)) {
-                                this->followMap[nonTerminal].insert(nextSymbol);
-                                break;
-                            }
-                            bool nextHasEpsillonFirst = false;
-                            auto nextFirstSet = this->firstMap[nextSymbol];
-                            for(auto firstSymbol : nextFirstSet) {
-                                if(!isEpsillon(firstSymbol.first)) {
-                                    this->followMap[nonTerminal].insert(firstSymbol.first);
-                                }
-                                if(isEpsillon(firstSymbol.first)) {
-                                    nextHasEpsillonFirst = true;
-                                }
-                            }
-                            if(!nextHasEpsillonFirst) break;
-                            if(j == production.size() - 1) {
-                                if(lhsOfRule == nonTerminal) break;
-                                computeFollow(lhsOfRule);
-                                for(auto follow : this->followMap[lhsOfRule]) {
-                                    this->followMap[nonTerminal].insert(follow);
-                                }
-                            }
-                        }
+                        handleFollow(rule.first, nonTerminal, production, i + 1);
                     }
                 }
             }
         }
     }
+    this->isFollowSet[nonTerminal] = true;
+}
+void FirstFollowBuilder::handleFollowLast(std::string lhsNonterminal, std::string rhsNonterminal) {
 
+    if(lhsNonterminal == rhsNonterminal)    return;
+    // check lw el lhsOfRule mwgod 3ndy a5er wa7ed
+    if(!this->isFollowSet[lhsNonterminal]) computeFollow(lhsNonterminal);
+    for(auto follow : this->followMap[lhsNonterminal]) {
+        this->followMap[rhsNonterminal].insert(follow);
+    }                    
+}
+
+void FirstFollowBuilder::handleFollow(std::string lhsNonterminal, std::string rhsNonterminal, std::vector<std::string> productions, int index) {
+    for(int j = index; j < productions.size(); j++) {
+        std::string nextSymbol = productions[j];
+        if(isTerminalString(nextSymbol)) {
+            this->followMap[rhsNonterminal].insert(nextSymbol);
+            break;
+        }
+        bool nextHasEpsillonFirst = false;
+        auto nextFirstSet = this->firstMap[nextSymbol];
+        for(auto firstSymbol : nextFirstSet) {
+            if(!isEpsillon(firstSymbol.first)) {
+                this->followMap[rhsNonterminal].insert(firstSymbol.first);
+            }
+            if(isEpsillon(firstSymbol.first)) {
+                nextHasEpsillonFirst = true;
+            }
+        }
+        if(!nextHasEpsillonFirst) break;
+        if(j == productions.size() - 1) {
+            if(lhsNonterminal == rhsNonterminal) break;
+            if(!this->isFollowSet[lhsNonterminal]) computeFollow(lhsNonterminal);
+            for(auto follow : this->followMap[lhsNonterminal]) {
+                this->followMap[rhsNonterminal].insert(follow);
+            }
+        }
+    }
 }
 
 bool FirstFollowBuilder::isTerminalString(std::string str) {
--- src/first_follow_builder.h
+++ src/first_follow_builder.h
@@ -9,17 +9,19 @@
 class FirstFollowBuilder
 {
 private:
-
     std::map<std::string, std::set<std::pair<std::string, std::vector<std::string>>>> firstMap;   
     std::map<std::string, std::set<std::string>> followMap;
     std::set<std::string> nonTerminals;
     std::set<std::string> terminals;
     std::map<std::string, std::vector<std::vector<std::string>>> productions;
+    std::map<std::string, bool> isFollowSet;
     void computeFirst(const std::string);
     void computeFollow(const std::string);
     bool isTerminalString(std::string str);
     bool isEpsillon(std::string str);
-
+    void handleFollowLast(std::string lhsNonterminal, std::string rhsNonterminal);
+    void handleFollow(std::string lhsNonterminal, std::string rhsNonterminal, std::vector<std::string> productions, int index);
+    
 public:
     FirstFollowBuilder(std::map<std::string, std::vector<std::vector<std::string>>> productions, std::set<std::string> nonTerminals);
     std::map<std::string, std::set<std::pair<std::string, std::vector<std::string>>>> getFirst();   
